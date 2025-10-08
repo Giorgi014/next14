1a7ea7a4c7fab7e37564588a5abdf7e613d129bb
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-08 20:35:59 +0400

    project update

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -6,4 +6,17 @@ export const users = [
 export async function GET() {
   return Response.json(users);
 }
-export async function POST() {}
+export async function POST(request: Request,) {
+  const user = await request.json();
+  const newUser = {
+    id: users.length + 1,
+    name: users.name,
+  };
+  users.push(newUser);
+  return new Response(JSON.stringify(newUser), {
+    headers: {
+      "COntent-Type": "application/json",
+    }["COntent-Type"],
+    status: 201,
+  });
+}
